aaae52e889c4b37793dfcc8d3ef9b8f40acc0608
ahmedameen <ahmed_ameen_elfiky@hotmail.com>
2018-08-21 02:32:13 +0200

    -Get all food trucks data near the location specifeid in the maps. -Add marker for each food truck.

--- SF_FoodTrucks/static/scripts/mapsManager.js
+++ SF_FoodTrucks/static/scripts/mapsManager.js
@@ -2,6 +2,10 @@ sfLat = 37.773972
 sfLng = -122.431297
 var map;
 var markers = [];
+var currentLocation = { lat: sfLat, lng: sfLng };
+
+foodTruckIconURL = '/static/icons/food-truck-32.png'
+foodCartIconURL = '/static/icons/food-cart-32.png'
 function initMap() {
     var mapOptions = {
         center: new google.maps.LatLng(sfLat, sfLng),
@@ -14,18 +18,19 @@ function initMap() {
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchBoxInput);
 
     var searchBox = new google.maps.places.SearchBox(searchBoxInput);
-    searchBox.addListener('places_changed', function () { map.setZoom(13);map.setCenter(searchBox.getPlaces()[0].geometry.location); addMarker(searchBox.getPlaces()[0].geometry.location); })
+    searchBox.addListener('places_changed', handleLocationChanged)
 
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
     });
 }
 
-function addMarker(location, locationTitle, OnClickEventHandler) {
+function addMarker(location, locationTitle, iconURL, OnClickEventHandler) {
     var marker = new google.maps.Marker({
         position: location,
         map: map,
-        title: locationTitle
+        title: locationTitle,
+        icon: iconURL
     });
     marker.addListener('click', OnClickEventHandler);
     markers.push(marker);
@@ -46,3 +51,28 @@ function createSearchBox() {
     return searchBox;
 }
 
+
+function handleLocationChanged() {
+    removeAllMarkers();
+    map.setZoom(14);
+    currentLocation = this.getPlaces()[0].geometry.location;
+    var placeTitle = this.getPlaces()[0].name;
+    map.setCenter(currentLocation);
+    addMarker(currentLocation, placeTitle);
+    getFoodTruckNearLocation(currentLocation.lat, currentLocation.lng);
+}
+
+
+function getFoodTruckNearLocation(locationLat, locationLng) {
+    $.get('/GetFoodTrucksNearLocation', { 'locationLat': locationLat, 'locationLng': locationLng, 'maxDistanceInMeters': 750 }, addMarkersToFoodTruckLocations)
+}
+
+function addMarkersToFoodTruckLocations(data) {
+    var trucks = JSON.parse(data);
+    for (var i = 0; i < trucks.length; i++) {
+        var truckLocation = { lat: trucks[i].location.coordinates[1], lng: trucks[i].location.coordinates[0] };
+        var iconURL = trucks[i].facilitytype == 'Push Cart' ? foodCartIconURL : foodTruckIconURL;
+        var truckName = trucks[i].applicant;
+        addMarker(truckLocation, truckName, iconURL);
+    }
+}
