2bf0e57a2df8567493068e0ec35873a73c4ff69e
ahmedameen <ahmed_ameen_elfiky@hotmail.com>
2018-08-24 01:20:56 +0200

    -Add script to initialize a database to store the useres info. -Setup the connection to the database. -Add authentication blue print. -Add login/register html templates.

--- SF_FoodTrucks/__init__.py
+++ SF_FoodTrucks/__init__.py
@@ -1,10 +1,16 @@
 """
 The flask application package.
 """
-
 from flask import Flask
+from SF_FoodTrucks import db
+
 app = Flask(__name__,instance_relative_config=True)
 app.config.from_pyfile('config.py')
+db.initApp(app)
+
+from SF_FoodTrucks import auth
+
+app.register_blueprint(auth.authBP)
 
 import SF_FoodTrucks.views
 import SF_FoodTrucks.foodTrucksAPI
---
+++ SF_FoodTrucks/auth.py
@@ -0,0 +1,79 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from SF_FoodTrucks.db import getDB
+
+authBP = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@authBP.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = getDB()
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+                'SELECT ID FROM Users WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO users (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@authBP.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = getDB()
+        error = None
+        user = db.execute(
+            'SELECT * FROM users WHERE Username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['userID'] = user['ID']
+            return redirect(url_for('home'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@authBP.before_app_request
+def loadLoggedInUser():
+    userID = session.get('userID')
+    if userID is None:
+        g.user = None
+    else:
+        g.user = getDB().execute(
+            'SELECT * FROM users WHERE ID = ?', (userID,)
+        ).fetchone()
+
+
+@authBP.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
